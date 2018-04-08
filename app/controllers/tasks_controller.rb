4dd1709ec18836375b1c65af8ff61d9feadef7e5
Keiichiro Takahashi <k16takahashi125@gmail.com>
2018-04-08 18:32:28 +0900

    Modify SCSS

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+class TasksController < ApplicationController
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  resources :tasks, only: [:create, :edit, :update]
+  # 検索機能
+  get 'search' => 'tasks#search'
 end
